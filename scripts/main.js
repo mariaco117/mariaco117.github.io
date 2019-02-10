98cc161e05a149409fb86cedebe087121251a4bb
mariaco117 <47491739+mariaco117@users.noreply.github.com>
2019-02-10 03:32:34 -0300

    Add files via upload

---
+++ scripts/main.js
@@ -0,0 +1,37 @@
+var myHeading = document.querySelector('h1');
+myHeading.textContent = 'Caca';
+
+var myImage = document.querySelector('img');
+
+myImage.onclick = function() {
+    var mySrc = myImage.getAttribute('src');
+    if(mySrc === 'images/weeb.gif') {
+      myImage.setAttribute ('src','images/dank.jpg');
+    } else {
+      myImage.setAttribute ('src','images/weeb.gif');
+    }
+}
+
+var myButton = document.querySelector('button');
+var myHeading = document.querySelector('h1');
+
+function setUserName() {
+    var myName = prompt('Please enter your name.');
+    localStorage.setItem('name', myName);
+    myHeading.textContent = 'Caca, ' + myName;
+}
+
+if(!localStorage.getItem('name')) {
+    setUserName();
+} else {
+    var storedName = localStorage.getItem('name');
+  myHeading.textContent = 'Caca, ' + storedName;
+}
+
+myButton.onclick = function() {
+    setUserName();
+}
+
+/*document.querySelector('img').onclick = function() {
+    alert('Ouch! Stop poking me!');
+}*/
